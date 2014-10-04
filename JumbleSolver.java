0763366e40030dc87170c7c0b76e84fcc3f16d63
Vishnu Indukuri <vishnui@gmail.com>
2014-10-04 18:18:22 -0500

    command line functionality

--- JumbleSolver.java
+++ JumbleSolver.java
@@ -1,42 +1,231 @@
-/* 
- * Author - Vishnu Indukuri
- * This is a fairly simple program that takes
- * as an input a string of letters and returns
- * a list of english words that can be made 
- * using each given letter only once.
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * Author - Vishnu Indukuri 
+ * This is a fairly simple program that takes as 
+ * input a string of letters and returns a list of english words that can be
+ * made using each given letter only once.
  * 
- * Obviously, there are a great number of words
- * in the English language; some words can be 
- * spelled in multiple ways, spellings differ 
- * between dialects, and there are plural and 
- * possessive versions of words so some 
- * optimization and pruning of the word list was
- * necessary to keep this program from outputing
- * eight different spellings for every word.  As 
- * a result, I cut out the less used spellings, 
- * the british and canadian versions, 
- * contractions, abbreviations, and a large
- * number of words that are almost never used or 
- * aren't even considered words anymore (the 80
- * -95 lists in the given SCOWL dump).
+ * Obviously, there are a great number of words in the English language; some
+ * words can be spelled in multiple ways, spellings differ between dialects, and
+ * there are plural and possessive versions of words so some optimization and
+ * pruning of the word list was necessary to keep this program from outputing
+ * eight different spellings for every word. As a result, I cut out the less
+ * used spellings, the british and canadian versions, contractions,
+ * abbreviations, and a large number of words that are almost never used or
+ * aren't even considered words anymore (the 70-95 lists in the given SCOWL
+ * dump).
  * 
- * If you need more or less words to be added,
- * unpack the included archive, read the README
- * and add the files you want to the words directory!
- *  In the future, I'll add command line options 
- * to configure the level of words you want included 
- * at runtime.  However, for 99.9% of the time, 
- * the current config will work great.
- *
- * Anyway, here goes the program.
+ * For explanation on how this program works, read the README.
  */
- 
- public class UnJumbler {
- 
-    /*
-     * Main method of the program.  Exec starts here.
-     */
-    public static void main(String[] args){
-        
-    }
-} 
+
+public class JumbleSolver {
+	/**
+	 * Main method of the program. Exec starts here.
+	 */
+	public static void main(String[] args) {
+		JumbleSolver js = new JumbleSolver();
+		if(args.length < 1){
+			js.startInteractiveSession();
+		}
+		else { // else process each argument
+			for(String argument: args){
+				long hash = js.calcHash(argument);
+				if(hash != -1){
+					StringBuffer results = js.getHashDivisbleWords(hash) ;
+					System.out.println(argument.toUpperCase()+": "+results.toString()) ;
+				} else  {
+					System.out.println(argument.toUpperCase()+" IS INVALID. " +
+							"PLEASE USE ONLY ENGLISH LETTERS.") ;
+				}
+			}
+		}
+	}
+
+	private BufferedWriter hashFile;
+	private long[] primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+			43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101 };
+	
+	private static final String emptyToExit = "TYPE EMPTY INPUT TO EXIT." ;
+	private static final String welcome= "GREETINGS PROFESSOR FALKEN. SHALL WE PLAY A GAME?" +
+			"\nTYPE A STRING TO CONTINUE: " ;
+	private static final String prompt = "TYPE A STRING TO CONTINUE: ";
+	private static final String bye = "GOODBYE." ;
+	private static final String hashFileName = "wordsWithHashes.txt" ;
+	private static final String wordsDirectory = "words/" ;
+	
+	/**
+	 * Constructor. Creates hashfile.
+	 * 
+	 * @throws IOException
+	 */
+	public JumbleSolver() {
+		// Init our custom hash file
+		try {
+			hashFile = new BufferedWriter(new FileWriter(new File(hashFileName), false));
+		} catch (IOException e) {
+			// WTF? exit.
+			e.printStackTrace();
+			System.out.println("Cannot open hash file!  Do I have permission?");
+			System.exit(100);
+		}
+
+		// Get list of files to read
+		// AKA files in words directory
+		File folder = new File(wordsDirectory);
+		File[] listOfFiles = folder.listFiles();
+		// Read each file in the directory
+		for (File file : listOfFiles) {
+			if (file.isFile()) {
+				readFile(file);
+			}
+		}
+
+		// Close output file. 
+		try {
+			hashFile.close();
+		} catch (IOException e) { e.printStackTrace();}
+	}
+
+	/**
+	 * Reads a file, calculating a hash for every word and writing 
+	 * back to output file. Format: "word,hash\n"
+	 * 
+	 * @param file A word list file.
+	 */
+	private void readFile(File file) {
+		StringBuffer hashBuffer = new StringBuffer();
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			for (String line; (line = br.readLine()) != null;) {
+				long hash = calcHash(line);
+				// If word read is a contraction or
+				// is invalid, ignore it.
+				if (hash == -1)
+					continue;
+				// buffer into stringbuffer for
+				// performance
+				hashBuffer.append(line + "," + hash + "\n");
+			}
+			// write it out only once per file.
+			// for performance.
+			hashFile.write(hashBuffer.toString());
+		} catch (FileNotFoundException e) {
+			e.printStackTrace(); // nothing we can do about it
+			System.out.print("Looks like I don't have permission to read "+file.toString()) ;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * Blocks.  Sits around waiting for user input. 
+	 * Empty input ends program.
+	 */
+	public void startInteractiveSession() {
+		String inputString;
+		Scanner inputScanner = new Scanner(System.in);
+		// Prompt for input 
+		System.out.println(emptyToExit) ;
+		System.out.print(welcome);
+		while ((inputString = inputScanner.nextLine()) != null) {
+			long hash = calcHash(inputString);
+			// empty input ends program
+			if (hash == 1)
+				break;
+			
+			if(hash == -1) {
+				System.out.println(inputString.toUpperCase()+" IS INVALID. " +
+						"PLEASE USE ONLY ENGLISH LETTERS.") ;
+			} else {
+				StringBuffer buffer = getHashDivisbleWords(hash);
+				System.out.print(buffer.toString() + "\n");
+			}
+			// Prompt for next input
+			System.out.print(prompt);
+		}
+
+		// Clean up file resources
+		inputScanner.close();
+		System.out.println(bye) ;
+	}
+
+	/** 
+	 * The search function.  Takes a hash and 
+	 * looks for words in our hash file with 
+	 * hashes that are divisible.
+	 * @param jumbledStringHash Hash of jumbled string
+	 * @return StringBuffer comma separated list of words  
+	 */
+	public StringBuffer getHashDivisbleWords(long jumbledStringHash) {
+		String wordcommahash;
+		StringBuffer buffer = new StringBuffer();
+		// open a read stream to our hash file.
+		try (BufferedReader hashes = new BufferedReader(new FileReader(
+				new File(hashFileName)));) {
+			// Loop through file
+			while ((wordcommahash = hashes.readLine()) != null) {
+				String[] wordhashCouple = wordcommahash.split(",");
+				if(wordhashCouple[0].length() < 2 && !wordhashCouple[0].equals("a"))
+					continue ;
+				long wordHash = Long.parseLong(wordhashCouple[1]);
+				// If divisible, then the word can be made 
+				// with input letters
+				if (jumbledStringHash % wordHash == 0)
+					buffer.append(wordhashCouple[0]+",");
+			}
+			hashes.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		// Get rid of that last comma
+		// Just to be a little cleaner
+		int lastComma = buffer.lastIndexOf(",") ;
+		if(lastComma != -1) buffer.deleteCharAt(lastComma);
+		return buffer ;
+	}
+	
+	/**
+	 * Calculates prime number hash of string. If line is null, line
+	 * contains an apostrophe or anything but letters then this
+	 * returns -1 (so that contractions, abbrevs., and  possessive 
+	 * forms of real words don't show up in our hash file).
+	 * 
+	 * Plural forms will still show up. That is an optimization I will make in
+	 * the future. TODO
+	 * 
+	 * @param line String to be hashed
+	 * @return the hash
+	 */
+	public long calcHash(String line) {
+		if (line == null){
+			return -1;
+		}
+		
+		long hash = 1;
+		line = line.toLowerCase(); // For comparision simplicity
+		for (int i = 0; i < line.length(); i++) {
+			char character = line.charAt(i);
+			// ignore possesives
+			if (character == '\'')
+				return -1;
+
+			int letter = character - 'a';
+			// english words may only have the 26 
+			// letters in the alphabet.  
+			if (letter < 0 || letter > 25)
+				return -1;
+			// Hyphenated words are apparently
+			// actually spelled in dictionaries without the
+			// hyphen so we don't need to worry about that
+			hash = hash * primes[letter];
+		}
+		return hash;
+	}
+}
--- README.md
+++ README.md
@@ -4,14 +4,11 @@ WUJumblr
 A word jumble solver.  Takes a series of letters as an input and returns a list of words that could be made 
 using only the letters in the input.
 
-To compile, navigate to this directory in your shell and type in "javac JumbleSolver.java".
-To run, type "java JumbleSolver"
-
 The Fundamental Theorem of Arithmetic (http://en.wikipedia.org/wiki/Fundamental_theorem_of_arithmetic)
 says that all numbers have one and only one unique prime factorization.  Which
-means that given any positive number, there is only one sequence of prime numbers that when multiplied give
-that number.  Which means that if you multiply a sequence of prime numbers, you will get a number that you cannot 
-get by multiplying any other sequence of prime numbers.  So if we represent the alphabet with the first 26 prime numbers,
+means that given any positive number, there is only one set of prime numbers that when multiplied give
+that number.  Which means that if you multiply a set of prime numbers, you will get a number that you cannot 
+get by multiplying any other set of prime numbers.  So if we represent the alphabet with the first 26 prime numbers,
 multiplying each letter in a string will give us a hash that is guaranteed to be unique to that set of letters. 
 
 The beauty of this system is that if we run into a string that uses a subset of our string's letters, then our hash must 
@@ -19,9 +16,16 @@ be a multiple of its hash. For example, the hash of "dog" must be the hash of "d
 defined our hash function to work; by multiplying prime number representatives of each letter of the string.  And since 
 multiplication is commutative, the order of the letters doesn't matter to the program either.
 
-As such, to solve the jumbled string puzzle, just look through the English word list hashing each 
-word and see if the jumbled string is divisible by the word.  
+So to solve the jumbled string puzzle, this program goes through the all the words in the list, hashes them, and stores each 
+"word,hash" pair in a hash file once at the beginning of the program.  Then, when the user enters an input, it divides the 
+jumbled string's hash by each hash in the hash file and the ones who return a remainder of 0 are printed.
 
-This program goes through the all the words in the list, hashes them, and stores each "word,hash" pair in a hash file once at 
-the beginning of the program.  Then, when the user enters an input, it mods the jumbled string's hash with each hash
-in the hash file and the ones who return a remainder of 0 are printed.
+The program is O(n) in the length of dictionary.  Technically multiplying the prime representatives of the strings together
+is not O(1) but words are usually short enough that for all practical purposes, it is.
+
+To compile, navigate to this directory in your shell and type in "javac JumbleSolver.java".
+For an interactive session, type "java JumbleSolver".
+To use as a command line utility, type "java JumbleSolver strings" where strings is a space separated list of jumbled strings to be 
+solved by the program.  
+
+Congratulations, you are now unstoppable at Words With Friends/Scrabble/most word games.
